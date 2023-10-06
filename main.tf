623e386156ce3b24a64151b0025bfe3d5bca92d6
Leandro M de Moraes <lmmoraes@gmail.com>
2023-10-06 15:39:04 -0400

    terraform code

--- main.tf
+++ main.tf
@@ -19,9 +19,9 @@ resource "azurerm_resource_group" "lmomessoDevops" {
   location = "East US"
 }
 
-# resource "azurerm_virtual_network" "lmomessoDevops" {
-#   name                = "lmomesso-network"
-#   address_space       = ["10.0.0.0/16"]
-#   location            = azurerm_resource_group.lmomessoDevops.location
-#   resource_group_name = azurerm_resource_group.lmomessoDevops.name
-# }
+resource "azurerm_virtual_network" "lmomessoDevops" {
+  name                = "lmomesso-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.lmomessoDevops.location
+  resource_group_name = azurerm_resource_group.lmomessoDevops.name
+}
