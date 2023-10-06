17bae8cfaf55c671983853c70d81606d8ee58d5e
Leandro M de Moraes <lmmoraes@gmail.com>
2023-10-06 11:16:21 -0400

    terraform devops

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name   = "lmomessoDevops"
+    resource_group_name   = "lmomessoTerraformDevops"
     storage_account_name  = "lmomessostates"
     container_name        = "tstate"
     # key                   = "+alMIMUa4YGQehArEYXLQD8+clSDgFcxDMbZAHflEPTxqDQGyhmsM5rvUHcLdKfALBXjuSeQGqyR+AStrt+YoA=="
@@ -14,14 +14,14 @@ terraform {
 }
 
 
-resource "azurerm_resource_group" "lmomessoDevops" {
-  name     = "lmomessoDevops"
+resource "azurerm_resource_group" "lmomessoTerraformDevops" {
+  name     = "lmomessoTerraformDevops"
   location = "East US"
 }
 
 resource "azurerm_virtual_network" "lmomessoDevops" {
   name                = "lmomesso-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.lmomessoDevops.location
-  resource_group_name = azurerm_resource_group.lmomessoDevops.name
+  location            = azurerm_resource_group.lmomessoTerraformDevops.location
+  resource_group_name = azurerm_resource_group.lmomessoTerraformDevops.name
 }
